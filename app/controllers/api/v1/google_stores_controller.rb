1849089ff2e0d0cc661fe8bdfb0fec19b53cceff
Randsson Silva <randsson.silva@locaweb.com.br>
2022-06-05 22:11:55 -0300

    add methods for Google Stores controller

--- app/controllers/api/v1/google_stores_controller.rb
+++ app/controllers/api/v1/google_stores_controller.rb
@@ -1,2 +1,10 @@
 class Api::V1::GoogleStoresController < ApplicationController
+  def index
+    places = GetGoogleRestaurantListService.new(params[:latitude], params[:longitude]).call
+    render json: places
+  end
+
+  def show
+    render json: GetGoogleRestaurantDetailsService.new(params[:id]).call
+  end
 end
