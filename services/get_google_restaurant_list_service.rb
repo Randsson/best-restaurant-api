819a5ac265b9b87337c172afbc02c71f52e78fa6
Randsson Silva <randsson.silva@locaweb.com.br>
2022-06-05 22:28:56 -0300

    refactor base_url for a better result

--- services/get_google_restaurant_list_service.rb
+++ services/get_google_restaurant_list_service.rb
@@ -12,7 +12,7 @@ class GetGoogleRestaurantListService
       key = "key=#{Rails.application.credentials.google_secret_key}"
       location = "location=#{@latitude},#{@longitude}"
       radius = "radius=5000"
-      base_url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants&#{location}&#{radius}&#{key}"
+      base_url = "https://maps.googleapis.com/maps/api/place/textsearch/json?type=restaurant&#{location}&#{radius}&#{key}"
       response = RestClient.get base_url
       JSON.parse(response.body)
     rescue RestClient::ExceptionWithResponse =>
